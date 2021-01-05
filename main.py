238e7ca7ce1b18a41d49eae0bb4336de6e24bb7b
Connor Sullivan <crsullivan@protonmail.com>
2021-01-04 22:46:00 -0700

    algo progress

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random as rnd
 import stable as stable
+import bolus as bolus
+
+running = True
 
 # Main Program
 print("\nWelcome to Better Bolus!\n")
@@ -12,10 +15,21 @@ x = [0.00, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 1.0
 # corresponding y axis values 
 y = [] 
 
+# setting y ranges
+plt.ylim(20, 600)
+
 # bolus profile values as an object of arrays, each with two values, the first being the amount of insulin in units, and the second being the amount of time in minutes between now and when the bolus would be taken
 bolusProfile = {}
 
-validatedTrendingAnswer = []
+totalOnBoardInsulin = {}
+
+# naming the x axis 
+plt.xlabel('Time (hrs)') 
+# naming the y axis 
+plt.ylabel('BG (mg/dl)') 
+
+
+validatedBoolAnswer = []
 validatedTrendingInfo = []
 
 def checkBGInput(bgInput):
@@ -27,13 +41,13 @@ def checkBGInput(bgInput):
     else: 
         y.append(bgInput)
 
-def checkTrendingAnswer(trendingAnswer):
-    if trendingAnswer != 'y' and trendingAnswer != 'n':
-        print("\nInvalid Input\n \nYou must enter either 'n' for No or 'y' for Yes. Do you know how your current BG is trending?")
-        trendingAnswer = input()
-        checkTrendingAnswer(trendingAnswer)
+def checkBoolAnswer(boolAnswer):
+    if boolAnswer != 'y' and boolAnswer != 'n':
+        print("\nInvalid Input\n \nYou must enter either 'n' for No or 'y' for Yes")
+        boolAnswer = input()
+        checkBoolAnswer(boolAnswer)
     else: 
-        validatedTrendingAnswer.append(trendingAnswer)
+        validatedBoolAnswer.append(boolAnswer)
 
 def checkTrendingInfo(trendingInfo):
     if trendingInfo != 'rising fast' and trendingInfo != 'rising' and trendingInfo != 'stable' and trendingInfo != 'dropping' and trendingInfo != 'dropping fast':
@@ -44,16 +58,16 @@ def checkTrendingInfo(trendingInfo):
     else: 
         validatedTrendingInfo.append(trendingInfo)
 
-while True:
+while running == True:
 
     print("Please enter a Blood Glucose value between 120-499")
     bgInput = int(input())
     checkBGInput(bgInput)
     print(f"initial bg: {y[0]}mg/dl. Do you know how your current BG is trending? \nEnter 'y' for Yes or 'n' for No. If you aren't sure, enter 'n'.")
-    trendingAnswer = input()
-    checkTrendingAnswer(trendingAnswer)
-    print(f"You answered {validatedTrendingAnswer[0]}")
-    if validatedTrendingAnswer[0] == "y":
+    boolAnswer = input()
+    checkBoolAnswer(boolAnswer)
+    print(f"You answered {validatedBoolAnswer[-1]}")
+    if validatedBoolAnswer[0] == "y":
         print("Please enter: \n'rising fast' for quickly rising BG \n'rising' for gently rising BG \n'stable' for stable BG \n'dropping' for dently dropping BG \n'dropping fast' for quickly dropping BG")
         trendingInfo = input()
         checkTrendingInfo(trendingInfo)
@@ -67,23 +81,44 @@ while True:
                 stableVariance = rnd.randint(-5, 5)
                 y.append(y[-1] + stableVariance)
             print(y)
-    if validatedTrendingAnswer[0] == "n":
+            # giving a title to my graph 
+            plt.title('Predicted Unadjusted Blood Glucose')
+             # function to show the plot 
+             # plotting the points  
+            plt.plot(x, y) 
+            plt.show() 
+    if validatedBoolAnswer[-1] == "n":
         print("No trending info, proceeding")
-    
 
-    # setting y ranges
-    plt.ylim(20, 600)
+    
+    def addBolus():
+        print("Would you like to add a Bolus? Type 'y' for Yes or 'n' for No")
+        boolAnswer = input()
+        checkBoolAnswer(boolAnswer)
+        print(f"You answered {validatedBoolAnswer[-1]}")
+        if validatedBoolAnswer[-1] == "y":
+            print("Please enter how many units of fast acting insulin to bolus")
+            bolus = int(input())
+            print("Please enter how many minutes from now the bolus will be applied")
+            time = float(input())
+            print(f"You added {bolus} units, to be applied in {time} minutes")
+            print(time)
+            bolusProfile.update({time: bolus})
+            print(bolusProfile)
+            addBolus()
+        if validatedBoolAnswer[-1] == "n":
+            exit
 
-    # plotting the points  
-    plt.plot(x, y) 
-    
-    # naming the x axis 
-    plt.xlabel('Time') 
-    # naming the y axis 
-    plt.ylabel('BG mg/dl') 
-    
-    # giving a title to my graph 
-    plt.title('Predicted Blood Glucose')
+    addBolus()
+
+    for key in bolusProfile:
+        for subkey in bolus.insulinProfile[key:]:
+            bolus.insulinProfile[subkey] *= bolusProfile[key]
+    print(bolus.insulinProfile)
+
+
+
+
     
     # function to show the plot 
     plt.show() 
