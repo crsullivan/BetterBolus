8da0c05aac48cdd61b96142d7627cdd382911c3a
Connor Sullivan <crsullivan@protonmail.com>
2021-01-11 18:54:59 -0700

    tkinter playing nicely with matplotlib and producing unadjusted profiles

--- app.py
+++ app.py
@@ -1,48 +1,85 @@
 import tkinter as tk
 from tkinter import filedialog, Text
+import main as main
+
+def raise_frame(frame):
+    frame.tkraise()
 
 root=tk.Tk() 
 root.title("Better Bolus V2.0")
 root.iconbitmap("./favicon.ico")
+
+f1 = tk.Frame(root)
+f2 = tk.Frame(root)
+f3 = tk.Frame(root)
+f4 = tk.Frame(root)
+
+for frame in (f1, f2, f3, f4):
+    frame.grid(row=0, column=0, sticky='news')
   
 # setting the windows size  
 root.geometry("600x400") 
    
-# declaring string variable 
-# for storing name and password 
-bg_var=tk.StringVar() 
-bolus_var=tk.StringVar() 
-bolusTime_var=tk.StringVar()
+# declaring string variables 
+bg_var = tk.StringVar() 
+trending_var = tk.StringVar()
+bolus_var = tk.StringVar() 
+bolusTime_var = tk.StringVar()
+
    
 # defining a function that will 
-# get the name and password and  
-# print them on the screen 
-def submit(): 
+# get and store data
+def submit_f1(): 
   
-    bg=bg_var.get() 
-    bolus=bolus_var.get() 
-    bolusTime=bolusTime_var.get()
+    bg = int(bg_var.get()) 
+    trending = trending_var.get()
       
-    print("bg is : " + bg) 
-    print("bolus amount is : " + bolus) 
-    print("bolus time is : " + bolusTime) 
+    print(f"bg is : {bg}") 
+    print("trending: " + trending) 
       
     bg_var.set("") 
-    bolus_var.set("") 
-    bolusTime_var.set("")
-      
+    trending_var.set("")   
+
+    main.initial_bg(bg)
+    main.unadjusted_graph(trending)
+    main.show_unadjusted_graph()
+
       
 # creating a label for  
 # bg using widget Label 
-bg_label = tk.Label(root, text = 'Blood Glucose', 
+bg_label = tk.Label(f1, text = 'Blood Glucose', 
                       font=('calibre', 
                             10, 'bold')) 
    
 # creating a entry for input 
 # bg using widget Entry 
-bg_entry = tk.Entry(root, 
+bg_entry = tk.Entry(f1, 
                       textvariable = bg_var,
                       font=('calibre',10,'normal')) 
+
+choices = { 'Stable','Rising','Dropping','Rising Fast','Dropping Fast', 'Unknown'}
+trending_var.set('Unknown') # set the default option
+
+trending_profile = tk.OptionMenu(f1, trending_var, *choices,)
+trending_label = tk.Label(f1, text="Choose a Trending Option")
+trending_profile.grid(row = 2, column =1)
+
+# on change dropdown value
+def change_dropdown(*args):
+    print( trending_var.get() )
+
+# link function to change dropdown
+trending_var.trace('w', change_dropdown)
+
+# # creating a label for trending 
+# trending_label = tk.Label(root, 
+#                        text = 'Trending', 
+#                        font = ('calibre',10,'bold')) 
+   
+# # creating a entry for trending 
+# trending_entry=tk.Entry(root, 
+#                      textvariable = trending_var, 
+#                      font = ('calibre',10,'normal')) 
    
 # creating a label for bolus 
 bolus_label = tk.Label(root, 
@@ -66,20 +103,30 @@ bolusTime_entry=tk.Entry(root,
    
 # creating a button using the widget  
 # Button that will call the submit function  
-sub_btn=tk.Button(root,text = 'Submit', 
-                  command = submit) 
+sub_btn=tk.Button(f1,text = 'Submit', 
+                  command = submit_f1) 
    
 # placing the label and entry in 
 # the required position using grid 
 # method 
 bg_label.grid(row=0,column=0) 
 bg_entry.grid(row=0,column=1) 
-bolus_label.grid(row=1,column=0) 
-bolus_entry.grid(row=1,column=1) 
-bolusTime_label.grid(row=2,column=0)
-bolusTime_entry.grid(row=2,column=1)
-sub_btn.grid(row=3,column=1) 
+trending_label.grid(row=1,column=0)
+trending_profile.grid(row=1,column=1)
+# bolus_label.grid(row=1,column=0) 
+# bolus_entry.grid(row=1,column=1) 
+# bolusTime_label.grid(row=2,column=0)
+# bolusTime_entry.grid(row=2,column=1)
+sub_btn.grid(row=2,column=1) 
    
 # performing an infinite loop  
 # for the window to display 
-root.mainloop() 
+# root.mainloop() 
+
+# bg_label.grid(row=0,column=0) 
+# bg_entry.grid(row=0,column=1) 
+# bolus_label.grid(row=1,column=0) 
+# bolus_entry.grid(row=1,column=1)
+
+raise_frame(f1)
+root.mainloop()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 import random as rnd
+import tkinter as tk
+from tkinter import filedialog, Text
 import stable as stable
 import rising as rising
 import dropping as dropping
@@ -9,195 +11,173 @@ import dropping_fast as dropping_fast
 import validation as validation
 import bolus as bolus
 
-running = True
 
 # Main Program
-print("\nWelcome to Better Bolus!\n")
-print("THIS PROGRAM IS CURRENTLY FOR MODELING PURPOSES ONLY AND SHOULD NOT BE USED\n TO DETERMINE HOW TO MANAGE AN INDIVIDUAL'S BLOOD GLUCOSE\n")
-def runProgram():
-    while running == True:
-        # x axis values 
-        x = [0.08163265, 0.1632653, 0.24489795, 0.3265306, 0.40816325, 0.48979589999999995, 0.5714285499999999, 0.6530611999999999, 0.7346938499999999, 0.8163264999999998, 0.8979591499999998, 0.9795917999999998, 1.0612244499999999, 1.1428570999999998, 1.2244897499999998, 1.3061223999999998, 1.3877550499999998, 1.4693876999999997, 1.5510203499999997, 1.6326529999999997, 1.7142856499999997, 1.7959182999999996, 1.8775509499999996, 1.9591835999999996, 2.0408162499999998, 2.1224488999999997, 2.2040815499999997, 2.2857141999999997, 2.3673468499999997, 2.4489794999999996, 2.5306121499999996, 2.6122447999999996, 2.6938774499999996, 2.7755100999999995, 2.8571427499999995, 2.9387753999999995, 3.0204080499999995, 3.1020406999999994, 3.1836733499999994, 3.2653059999999994, 3.3469386499999993, 3.4285712999999993, 3.5102039499999993, 3.5918365999999993, 3.6734692499999992, 3.755101899999999, 3.836734549999999, 3.918367199999999, 3.999999849999999]
-        # corresponding y axis values list, values to be added by the program, starting with initial BG
-        y = [] 
-        # bolus profile values as an object of arrays, each with two values, the first being the amount of insulin in units, and the second being the amount of time in minutes between now and when the bolus would be taken
-        bolusProfile = {}
-        print("Please enter a Blood Glucose value between 120-499, or press 'e' to exit.")
-        bgInput = input()
-        if bgInput == 'e':
-            exit()
-        # insert current bg at start of y axis (current vlood glucose)
-        y.append(validation.checkBGInput(bgInput))
-        # check if user has trernding info
-        print(f"initial bg: {y[0]}mg/dl. Do you know how your current BG is trending? \nEnter 'y' for Yes or 'n' for No. If you aren't sure, enter 'n'.")
-        boolAnswer = input()
-        validatedBoolAnswer = validation.checkBoolAnswer(boolAnswer)
-        print(f"You answered {validatedBoolAnswer}")
-        if validatedBoolAnswer == "y":
-            # check how user is trending
-            print("Please enter: \n'rising fast' for quickly rising BG \n'rising' for gently rising BG \n'stable' for stable BG \n'dropping' for dently dropping BG \n'dropping fast' for quickly dropping BG")
-            trendingInfo = input()
-            validatedTrendingInfo = validation.checkTrendingInfo(trendingInfo)
-            print(f"You answered {validatedTrendingInfo}")
-            if validatedTrendingInfo == "stable":
-                # build the rest of the y axis for a stable profile
-                yBuild = (stable.stableBuild())
-                for i in range(len(yBuild)):
-                    y.append(y[i] + yBuild[i])
-            if validatedTrendingInfo == "rising":
-                # build the rest of the y axis for a rising profile
-                yBuild = (rising.risingBuild())
-                for i in range(len(yBuild)):
-                    y.append(y[i] + yBuild[i])
-            if validatedTrendingInfo == "dropping":
-                # build the rest of the y axis for a dropping profile
-                yBuild = (dropping.droppingBuild())
-                for i in range(len(yBuild)):
-                    y.append(y[i] + yBuild[i])
-            if validatedTrendingInfo == "rising fast":
-                # build the rest of the y axis for a rising_fast profile
-                yBuild = (rising_fast.rising_fastBuild())
-                for i in range(len(yBuild)):
-                    y.append(y[i] + yBuild[i])
-            if validatedTrendingInfo == "dropping fast":
-                # build the rest of the y axis for a dropping_fast profile
-                yBuild = (dropping_fast.dropping_fastBuild())
-                for i in range(len(yBuild)):
-                    y.append(y[i] + yBuild[i])
-        if validatedBoolAnswer == "n":
-            print("No trending info, proceeding with 'stable' assumption")
-            validatedTrendingInfo = "none"
-            # build the rest of the y axis for a stable profile
-            yBuild = (stable.stableBuild())
-            for i in range(len(yBuild)):
-                y.append(y[i] + yBuild[i])
+# print("\nWelcome to Better Bolus!\n")
+# print("THIS PROGRAM IS CURRENTLY FOR MODELING PURPOSES ONLY AND SHOULD NOT BE USED\n TO DETERMINE HOW TO MANAGE AN INDIVIDUAL'S BLOOD GLUCOSE\n")
 
-        print("Calculating predicted unadjusted Blood Glucose")
+# x axis values 
+x = [0.08163265, 0.1632653, 0.24489795, 0.3265306, 0.40816325, 0.48979589999999995, 0.5714285499999999, 0.6530611999999999, 0.7346938499999999, 0.8163264999999998, 0.8979591499999998, 0.9795917999999998, 1.0612244499999999, 1.1428570999999998, 1.2244897499999998, 1.3061223999999998, 1.3877550499999998, 1.4693876999999997, 1.5510203499999997, 1.6326529999999997, 1.7142856499999997, 1.7959182999999996, 1.8775509499999996, 1.9591835999999996, 2.0408162499999998, 2.1224488999999997, 2.2040815499999997, 2.2857141999999997, 2.3673468499999997, 2.4489794999999996, 2.5306121499999996, 2.6122447999999996, 2.6938774499999996, 2.7755100999999995, 2.8571427499999995, 2.9387753999999995, 3.0204080499999995, 3.1020406999999994, 3.1836733499999994, 3.2653059999999994, 3.3469386499999993, 3.4285712999999993, 3.5102039499999993, 3.5918365999999993, 3.6734692499999992, 3.755101899999999, 3.836734549999999, 3.918367199999999, 3.999999849999999]
+# corresponding y axis values list, values to be added by the program, starting with initial BG
+y = [] 
+# bolus profile values as an object of arrays, each with two values, the first being the amount of insulin in units, and the second being the amount of time in minutes between now and when the bolus would be taken
+bolusProfile = {}
+bgInput = ""
+# insert current bg at start of y axis (current blood glucose)
+def initial_bg(bg):
+    y.append(bg)
+# check if user has trernding info
+def unadjusted_graph(trending):
+    if trending == "Stable":
+        # build the rest of the y axis for a stable profile
+        yBuild = (stable.stableBuild())
+        for i in range(len(yBuild)):
+            y.append(y[i] + yBuild[i])
+    if trending == "Rising":
+        # build the rest of the y axis for a rising profile
+        yBuild = (rising.risingBuild())
+        for i in range(len(yBuild)):
+            y.append(y[i] + yBuild[i])
+    if trending == "Dropping":
+        # build the rest of the y axis for a dropping profile
+        yBuild = (dropping.droppingBuild())
+        for i in range(len(yBuild)):
+            y.append(y[i] + yBuild[i])
+    if trending == "Rising Fast":
+        # build the rest of the y axis for a rising_fast profile
+        yBuild = (rising_fast.rising_fastBuild())
+        for i in range(len(yBuild)):
+            y.append(y[i] + yBuild[i])
+    if trending == "Dropping Fast":
+        # build the rest of the y axis for a dropping_fast profile
+        yBuild = (dropping_fast.dropping_fastBuild())
+        for i in range(len(yBuild)):
+            y.append(y[i] + yBuild[i])
+    if trending == "Unknown":
+        print("No trending info, proceeding with 'Stable' assumption")
+        trending = "none"
+        # build the rest of the y axis for a stable profile
+        yBuild = (stable.stableBuild())
+        for i in range(len(yBuild)):
+            y.append(y[i] + yBuild[i])
 
-        # naming the x axis 
-        plt.xlabel('Time (hrs)') 
-        # naming the y axis 
-        plt.ylabel('BG (mg/dl)') 
-        # setting y ranges
-        plt.ylim(20, 600)
-        # giving a title to my graph 
-        plt.title('Predicted Unadjusted Blood Glucose') 
-        # plotting the points  
-        plt.plot(x, y) 
-        # function to show the plot
-        plt.show() 
+def show_unadjusted_graph():
+    # naming the x axis 
+    plt.xlabel('Time (hrs)') 
+    # naming the y axis 
+    plt.ylabel('BG (mg/dl)') 
+    # setting y ranges
+    plt.ylim(20, 600)
+    # giving a title to my graph 
+    plt.title('Predicted Unadjusted Blood Glucose') 
+    # plotting the points  
+    plt.plot(x, y) 
+    # function to show the plot
+    plt.show() 
 
-        def addBolus():
-            print("Would you like to add a Bolus? Type 'y' for Yes or 'n' for No")
-            boolAnswer = input()
-            validatedBoolAnswer = validation.checkBoolAnswer(boolAnswer)
-            print(f"You answered {validatedBoolAnswer}")
-            if validatedBoolAnswer[-1] == "y":
-                if len(bolusProfile) == 0:
-                    print("Please enter how many units of fast acting insulin to bolus immediately")
-                else: 
-                    print("Please enter how many units of fast acting insulin to bolus")
-                validatedBolus = validation.checkBolus(input())
-                if len(bolusProfile) == 0:
-                    validatedTimeInput = 0.00
-                else:
-                    print("Please enter how much time from now (divisible by 5) the bolus will be applied\nusing the following format: (hours).(minutes divisible by 5) for example: \nto represent one hour and fifteen minutes, 1.15 would be entered")
-                    validatedTimeInput = validation.checkTimeInput(input())
-                # validatedTimeInput = checkTimeInput(timeInput)
-                # print("time ver", checkTimeInput(timeInput))
-                stringFix = []
-                if len(str(validatedTimeInput)) == 3: 
-                    stringFix.append(0)
-                else:
-                    stringFix.append("")
-                print(f"You added {validatedBolus} units, to be applied in {str(validatedTimeInput)[0]} hours and {str(validatedTimeInput)[2:]}{stringFix[0]} minutes")
-                # print(timeInput)
-                bolusProfile.update({validatedTimeInput: validatedBolus})
-                # print(bolusProfile)
-                addBolus()
-            if validatedBoolAnswer == "n":
-                if len(bolusProfile) == 0:
-                    print("No bolus applied, returning to start of program")
-                    return runProgram()
-                else:
-                    print("Calculating adjusted Blood Glucose")
-                    exit
-
+def addBolus():
+    print("Would you like to add a Bolus? Type 'y' for Yes or 'n' for No")
+    boolAnswer = input()
+    validatedBoolAnswer = validation.checkBoolAnswer(boolAnswer)
+    print(f"You answered {validatedBoolAnswer}")
+    if validatedBoolAnswer[-1] == "y":
+        if len(bolusProfile) == 0:
+            print("Please enter how many units of fast acting insulin to bolus immediately")
+        else: 
+            print("Please enter how many units of fast acting insulin to bolus")
+        validatedBolus = validation.checkBolus(input())
+        if len(bolusProfile) == 0:
+            validatedTimeInput = 0.00
+        else:
+            print("Please enter how much time from now (divisible by 5) the bolus will be applied\nusing the following format: (hours).(minutes divisible by 5) for example: \nto represent one hour and fifteen minutes, 1.15 would be entered")
+            validatedTimeInput = validation.checkTimeInput(input())
+        # validatedTimeInput = checkTimeInput(timeInput)
+        # print("time ver", checkTimeInput(timeInput))
+        stringFix = []
+        if len(str(validatedTimeInput)) == 3: 
+            stringFix.append(0)
+        else:
+            stringFix.append("")
+        print(f"You added {validatedBolus} units, to be applied in {str(validatedTimeInput)[0]} hours and {str(validatedTimeInput)[2:]}{stringFix[0]} minutes")
+        # print(timeInput)
+        bolusProfile.update({validatedTimeInput: validatedBolus})
+        # print(bolusProfile)
         addBolus()
 
-        bolusList = sorted(bolusProfile.items())
+# addBolus()
 
-        insulinProfileList = sorted(bolus.insulinProfile.items())
+# bolusList = sorted(bolusProfile.items())
 
-        insulinEffect = []
-        
-        for i in range(len(insulinProfileList)):
-            insulinEffect.append(insulinProfileList[i][1] * bolusList[0][1])
+# insulinProfileList = sorted(bolus.insulinProfile.items())
 
-        def bolusStack():
-            if len(bolusList) > 1:
-                for i in range(len(bolusList) - 1):
-                    startTime = bolusList[i+1][0]
-                    # print('time', startTime)
-                    startIndex = list(bolus.insulinProfile.keys()).index(startTime)
-                    insulinEffectCopy = insulinEffect.copy()
-                    # print('slice', insulinEffect[startIndex:])
-                    del(insulinEffect[startIndex:])
-                    for subI in range(len(y[startIndex:])):
-                        # print(insulinEffectCopy[startIndex:][subI], ' to ', insulinEffectCopy[startIndex:][subI] + insulinProfileList[subI][1] * bolusList[i+1][1])
-                        insulinEffect.append(insulinEffectCopy[startIndex:][subI] + insulinProfileList[subI][1] * bolusList[i+1][1])
-                        # print(subI)
-        bolusStack()
+# insulinEffect = []
 
-        insulinEffectResistanceAdjusted = []
+# for i in range(len(insulinProfileList)):
+#     insulinEffect.append(insulinProfileList[i][1] * bolusList[0][1])
 
-        if validatedTrendingInfo:
-            if validatedTrendingInfo == "rising":
-                # build the rest of the y axis for a rising profile
-                for i in range(len(y)):
-                    resistanceFactor = (1 - rising.insulinResistanceAlgo(y[i]))
-                    insulinEffectResistanceAdjusted.append(((insulinEffect[i] * resistanceFactor) * 4))
-            if validatedTrendingInfo == "dropping":
-                # build the rest of the y axis for a dropping profile
-                for i in range(len(y)):
-                    resistanceFactor = (1 - dropping.insulinResistanceAlgo(y[i]))
-                    insulinEffectResistanceAdjusted.append(((insulinEffect[i] * resistanceFactor) * 4))
-            if validatedTrendingInfo == "rising fast":
-                # build the rest of the y axis for a rising_fast profile
-                for i in range(len(y)):
-                    resistanceFactor = (1 - rising_fast.insulinResistanceAlgo(y[i]))
-                    insulinEffectResistanceAdjusted.append(((insulinEffect[i] * resistanceFactor) * 4))
-            if validatedTrendingInfo == "dropping fast":
-                # build the rest of the y axis for a dropping_fast profile
-                for i in range(len(y)):
-                    resistanceFactor = (1 - dropping_fast.insulinResistanceAlgo(y[i]))
-                    insulinEffectResistanceAdjusted.append(((insulinEffect[i] * resistanceFactor) * 4))
+# def bolusStack():
+#     if len(bolusList) > 1:
+#         for i in range(len(bolusList) - 1):
+#             startTime = bolusList[i+1][0]
+#             # print('time', startTime)
+#             startIndex = list(bolus.insulinProfile.keys()).index(startTime)
+#             insulinEffectCopy = insulinEffect.copy()
+#             # print('slice', insulinEffect[startIndex:])
+#             del(insulinEffect[startIndex:])
+#             for subI in range(len(y[startIndex:])):
+#                 # print(insulinEffectCopy[startIndex:][subI], ' to ', insulinEffectCopy[startIndex:][subI] + insulinProfileList[subI][1] * bolusList[i+1][1])
+#                 insulinEffect.append(insulinEffectCopy[startIndex:][subI] + insulinProfileList[subI][1] * bolusList[i+1][1])
+#                 # print(subI)
+# # bolusStack()
 
-        if validatedTrendingInfo == "none" or validatedTrendingInfo == "stable":
-            for i in range(len(y)):
-                resistanceFactor = (1 - stable.insulinResistanceAlgo(y[i]))
-                insulinEffectResistanceAdjusted.append(((insulinEffect[i] * resistanceFactor) * 4))
+# insulinEffectResistanceAdjusted = []
 
-        yAdjusted = []
-            
-        for i in range(len(y)):
-            if i <= 2:
-                yAdjusted.append(y[i])
-            else:
-                yAdjusted.append(yAdjusted[i-1] - insulinEffectResistanceAdjusted[i])
+# if validatedTrendingInfo:
+#     if validatedTrendingInfo == "rising":
+#         # build the rest of the y axis for a rising profile
+#         for i in range(len(y)):
+#             resistanceFactor = (1 - rising.insulinResistanceAlgo(y[i]))
+#             insulinEffectResistanceAdjusted.append(((insulinEffect[i] * resistanceFactor) * 4))
+#     if validatedTrendingInfo == "dropping":
+#         # build the rest of the y axis for a dropping profile
+#         for i in range(len(y)):
+#             resistanceFactor = (1 - dropping.insulinResistanceAlgo(y[i]))
+#             insulinEffectResistanceAdjusted.append(((insulinEffect[i] * resistanceFactor) * 4))
+#     if validatedTrendingInfo == "rising fast":
+#         # build the rest of the y axis for a rising_fast profile
+#         for i in range(len(y)):
+#             resistanceFactor = (1 - rising_fast.insulinResistanceAlgo(y[i]))
+#             insulinEffectResistanceAdjusted.append(((insulinEffect[i] * resistanceFactor) * 4))
+#     if validatedTrendingInfo == "dropping fast":
+#         # build the rest of the y axis for a dropping_fast profile
+#         for i in range(len(y)):
+#             resistanceFactor = (1 - dropping_fast.insulinResistanceAlgo(y[i]))
+#             insulinEffectResistanceAdjusted.append(((insulinEffect[i] * resistanceFactor) * 4))
 
-        plt.xlabel('Time (hrs)') 
-        # naming the y axis 
-        plt.ylabel('BG (mg/dl)') 
-        plt.ylim(20, 600)
-        # plt.xticks(x, x)
-        plt.title('Predicted Blood Glucose Adjusted For Bolus')
-        # function to show the plot 
-        # plotting the points  
-        plt.plot(x, yAdjusted) 
-        plt.show() 
+# if validatedTrendingInfo == "none" or validatedTrendingInfo == "stable":
+#     for i in range(len(y)):
+#         resistanceFactor = (1 - stable.insulinResistanceAlgo(y[i]))
+#         insulinEffectResistanceAdjusted.append(((insulinEffect[i] * resistanceFactor) * 4))
 
-runProgram()
+# yAdjusted = []
+    
+# for i in range(len(y)):
+#     if i <= 2:
+#         yAdjusted.append(y[i])
+#     else:
+#         yAdjusted.append(yAdjusted[i-1] - insulinEffectResistanceAdjusted[i])
+
+# plt.xlabel('Time (hrs)') 
+# # naming the y axis 
+# plt.ylabel('BG (mg/dl)') 
+# plt.ylim(20, 600)
+# # plt.xticks(x, x)
+# plt.title('Predicted Blood Glucose Adjusted For Bolus')
+# # function to show the plot 
+# # plotting the points  
+# plt.plot(x, yAdjusted) 
+# plt.show() 
+
         
 
 
